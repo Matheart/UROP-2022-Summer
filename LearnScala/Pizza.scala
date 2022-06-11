5dcb3d624ef39d2b7b91c3830823021303beb3da
Matheart <waautomationwong@gmail.com>
2022-06-11 20:47:41 +0800

    Read until Functional programming

--- LearnScala/Pizza.scala
+++ LearnScala/Pizza.scala
@@ -8,6 +8,10 @@ enum CrustType:
 enum Topping:
     case Cheese, Pepperoni, BlackOlives, GreenOlives, Onions
 
+import CrustSize.*
+import CrustType.*
+import Topping.*
+
 case class Pizza(
     crustSize: CrustSize,
     crustType: CrustType,
@@ -34,10 +38,35 @@ def crustPrice(s: CrustSize, t: CrustType): Double =
         case (Large, Regular) => 0.75
         case (Large, Thick) => 1.00
 
-/*
+object PizzaService extends PizzaServiceInterface:
+    def price(p: Pizza): Double = p match
         case Pizza(crustSize, crustType, toppings) =>
             val base = 6.00
             val crust = crustPrice(crustSize, crustType)
             val tops = toppings.map(toppingPrice).sum
             base + crust + tops    
-*/
+
+    def addTopping(p: Pizza, t: Topping): Pizza = 
+        p.copy(toppings = p.toppings :+ t) // append
+    
+    def removeAllToppings(p: Pizza): Pizza = 
+        p.copy(toppings = Seq.empty)
+    
+    def updateCrustSize(p: Pizza, cs: CrustSize): Pizza = 
+        p.copy(crustSize = cs)
+    
+    def updateCrustType(p: Pizza, ct: CrustType): Pizza =
+        p.copy(crustType = ct)
+
+end PizzaService
+
+@main def PizzaApp =
+    import PizzaService.*
+
+    val p = Pizza(Small, Thin, Seq(Cheese))
+    val p1 = addTopping(p, Pepperoni)
+    val p2 = addTopping(p1, Onions)
+    val p3 = updateCrustType(p2, Thick)
+    val p4 = updateCrustSize(p3, Large)
+
+    println(price(p4))
